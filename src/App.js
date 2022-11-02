ca73e9dea03663b529dc92e5cd940d3a1f92d71b
fenrir <mlex.alba@gmail.com>
2022-11-02 11:49:13 -0400

    test pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> jenkins test.
+          Edit <code>src/App.js</code> one jenkins test.
         </p>
         <a
           className="App-link"
